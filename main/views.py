739c17a8a274273a887af6b3d54b566e5e3d66c5
Save1l <savel280408@gmail.com>
2025-03-15 19:07:22 +0300

    Add categories and products sql tables and fixtures for goods

--- main/views.py
+++ main/views.py
@@ -1,14 +1,21 @@
+from unicodedata import category
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.template import context
 from django.template.defaultfilters import first
 
+from goods.models import Categories
+
 
 
 def index(request):
+
+    categories = Categories.objects.all()
+
     context = {
         'title': 'Home',
         'content': 'Магазин мебели HOME',
+        'categories':categories,
     }
 
     return render(request, 'main/index.html', context)
