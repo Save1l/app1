2cd12cb048d5a273ead16e776a070f84fe067624
Save1l <savel280408@gmail.com>
2025-03-17 01:25:08 +0300

    Register simple tag for categories

--- goods/models.py
+++ goods/models.py
@@ -35,4 +35,14 @@ class Products(models.Model):
         verbose_name_plural = 'Продукты'
     
     def __str__(self):
-         return f'{self.name} Количество {self.quantity}'
+        return f'{self.name} Количество {self.quantity}'
+    
+    def display_id(self):
+        return f"{self.id:05}"
+    
+    def sell_price(self):
+        if self.discount:
+            return round(self.price*(1-self.discount/100),2)
+        
+        else:
+            return self.price
--- main/views.py
+++ main/views.py
@@ -10,12 +10,9 @@ from goods.models import Categories
 
 def index(request):
 
-    categories = Categories.objects.all()
-
     context = {
         'title': 'Home',
         'content': 'Магазин мебели HOME',
-        'categories':categories,
     }
 
     return render(request, 'main/index.html', context)
